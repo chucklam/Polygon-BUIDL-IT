c018eca066d9c6e2268b847712aa68b761685e3f
Chuck Lam <939400+chucklam@users.noreply.github.com>
2022-08-21 22:29:30 -0700

    Change syntax to use arrow functions

--- main.js
+++ main.js
@@ -38,24 +38,24 @@ const memory = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const semantics = arithmetic.createSemantics();
 semantics.addOperation('eval', {
   // Exp(e) { return e.eval() }, // This "pass-through action" is implied.
-  AddExp_plus(a, _, b) { return a.eval() + b.eval() },
-  AddExp_minus(a, _, b) { return a.eval() - b.eval() },
-  MulExp_times(a, _, b) { return a.eval() * b.eval() },
-  MulExp_div(a, _, b) { return a.eval() / b.eval() },
-  ExpExp_power(x, _, y)   { return Math.pow(x.eval(), y.eval()); },
-  PriExp_paren(_l, a, _r) { return a.eval(); },
+  AddExp_plus:  (a, _, b) => a.eval() + b.eval(),
+  AddExp_minus: (a, _, b) => a.eval() - b.eval(),
+  MulExp_times: (a, _, b) => a.eval() * b.eval(),
+  MulExp_div:   (a, _, b) => a.eval() / b.eval(),
+  ExpExp_power: (a, _, b) => Math.pow(a.eval(), b.eval()),
+  PriExp_paren: (_l, a, _r) => a.eval(),
 
-  MemExp_memory(_l, a, _r) { return memory[a.eval()] },
+  MemExp_memory: (_l, a, _r) => memory[a.eval()],
 
   number(_) { return parseInt(this.sourceString) },
 });
 
 const result = [
+  semantics(arithmetic.match('12345')).eval() == 12345,
   semantics(arithmetic.match('100 + 1 * 2')).eval() == 102,
   semantics(arithmetic.match('1 + 2 - 3 + 4')).eval() == 4,
   semantics(arithmetic.match('1 + 2 ^ 3')).eval() == 9,
   semantics(arithmetic.match('(1 + 2) ^ 3')).eval() == 27,
-  semantics(arithmetic.match('12345')).eval() == 12345,
-  semantics(arithmetic.match('m[0] + m[2]')).eval() == 4,
+  semantics(arithmetic.match('m[0] + 4 * m[2]')).eval() == 13,
 ];
 console.log(result);
